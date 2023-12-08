73a6ae35e98063425bbb582c0e2f5fef27c6eed2
Matthew Martelle <matthew@martelle.dev>
2023-12-08 12:53:16 -0700

    added keyboard control back in if it is selected

--- src/actions.rs
+++ src/actions.rs
@@ -43,54 +43,102 @@ pub fn gamepad_system(
     button_inputs: Res<Input<GamepadButton>>,
     button_axes: Res<Axis<GamepadButton>>,
     axes: Res<Axis<GamepadAxis>>,
+    keyboard_input: Res<Input<KeyCode>>,
     mut actions: ResMut<Actions>,
 ) {
-    for gamepad in gamepads.iter() {
-        if button_inputs.just_pressed(GamepadButton::new(gamepad, GamepadButtonType::South)) {
-            info!("{:?} just pressed South", gamepad);
-        } else if button_inputs.just_released(GamepadButton::new(gamepad, GamepadButtonType::South))
-        {
-            info!("{:?} just released South", gamepad);
-        }
+    match actions.input_device {
+        InputDevice::Gamepad => {
+            for gamepad in gamepads.iter() {
+                if button_inputs.just_pressed(GamepadButton::new(gamepad, GamepadButtonType::South))
+                {
+                    info!("{:?} just pressed South", gamepad);
+                } else if button_inputs
+                    .just_released(GamepadButton::new(gamepad, GamepadButtonType::South))
+                {
+                    info!("{:?} just released South", gamepad);
+                }
 
-        let right_trigger = button_axes
-            .get(GamepadButton::new(
-                gamepad,
-                GamepadButtonType::RightTrigger2,
-            ))
-            .unwrap();
-        if right_trigger.abs() > 0.01 {
-            info!("{:?} RightTrigger2 value is {}", gamepad, right_trigger);
-        }
+                let right_trigger = button_axes
+                    .get(GamepadButton::new(
+                        gamepad,
+                        GamepadButtonType::RightTrigger2,
+                    ))
+                    .unwrap();
+                if right_trigger.abs() > 0.01 {
+                    info!("{:?} RightTrigger2 value is {}", gamepad, right_trigger);
+                }
 
-        let left_stick_x = axes
-            .get(GamepadAxis::new(gamepad, GamepadAxisType::LeftStickX))
-            .unwrap();
-        if left_stick_x.abs() > 0.01 {
-            info!("{:?} LeftStickX value is {}", gamepad, left_stick_x);
-        }
-        let left_stick_y = axes
-            .get(GamepadAxis::new(gamepad, GamepadAxisType::LeftStickY))
-            .unwrap();
-        if left_stick_y.abs() > 0.01 {
-            info!("{:?} LeftStickX value is {}", gamepad, left_stick_y);
-        }
+                let left_stick_x = axes
+                    .get(GamepadAxis::new(gamepad, GamepadAxisType::LeftStickX))
+                    .unwrap();
+                if left_stick_x.abs() > 0.01 {
+                    info!("{:?} LeftStickX value is {}", gamepad, left_stick_x);
+                }
+                let left_stick_y = axes
+                    .get(GamepadAxis::new(gamepad, GamepadAxisType::LeftStickY))
+                    .unwrap();
+                if left_stick_y.abs() > 0.01 {
+                    info!("{:?} LeftStickX value is {}", gamepad, left_stick_y);
+                }
 
-        let right_stick_x = axes
-            .get(GamepadAxis::new(gamepad, GamepadAxisType::RightStickX))
-            .unwrap();
-        if right_stick_x.abs() > 0.01 {
-            info!("{:?} RightStickX value is {}", gamepad, right_stick_x);
+                let right_stick_x = axes
+                    .get(GamepadAxis::new(gamepad, GamepadAxisType::RightStickX))
+                    .unwrap();
+                if right_stick_x.abs() > 0.01 {
+                    info!("{:?} RightStickX value is {}", gamepad, right_stick_x);
+                }
+                let right_stick_y = axes
+                    .get(GamepadAxis::new(gamepad, GamepadAxisType::RightStickY))
+                    .unwrap();
+                if right_stick_y.abs() > 0.01 {
+                    info!("{:?} RightStickX value is {}", gamepad, right_stick_y);
+                }
+                actions.p1_movement.x = left_stick_x;
+                actions.p1_movement.y = left_stick_y;
+                actions.p1_bee_movement.x = right_stick_x;
+                actions.p1_bee_movement.y = right_stick_y;
+            }
         }
-        let right_stick_y = axes
-            .get(GamepadAxis::new(gamepad, GamepadAxisType::RightStickY))
-            .unwrap();
-        if right_stick_y.abs() > 0.01 {
-            info!("{:?} RightStickX value is {}", gamepad, right_stick_y);
+        InputDevice::Keyboard => {
+            if keyboard_input.pressed(KeyCode::D) && keyboard_input.pressed(KeyCode::A) {
+                actions.p1_movement.x = 0.0;
+            } else if keyboard_input.pressed(KeyCode::D) {
+                actions.p1_movement.x = 1.0;
+            } else if keyboard_input.pressed(KeyCode::A) {
+                actions.p1_movement.x = -1.0;
+            } else {
+                actions.p1_movement.x = 0.0;
+            }
+
+            if keyboard_input.pressed(KeyCode::W) && keyboard_input.pressed(KeyCode::S) {
+                actions.p1_movement.y = 0.0;
+            } else if keyboard_input.pressed(KeyCode::W) {
+                actions.p1_movement.y = 1.0;
+            } else if keyboard_input.pressed(KeyCode::S) {
+                actions.p1_movement.y = -1.0;
+            } else {
+                actions.p1_movement.y = 0.0;
+            }
+
+            if keyboard_input.pressed(KeyCode::Right) && keyboard_input.pressed(KeyCode::Left) {
+                actions.p1_bee_movement.x = 0.0;
+            } else if keyboard_input.pressed(KeyCode::Right) {
+                actions.p1_bee_movement.x = 1.0;
+            } else if keyboard_input.pressed(KeyCode::Left) {
+                actions.p1_bee_movement.x = -1.0;
+            } else {
+                actions.p1_bee_movement.x = 0.0;
+            }
+
+            if keyboard_input.pressed(KeyCode::Up) && keyboard_input.pressed(KeyCode::Down) {
+                actions.p1_bee_movement.y = 0.0;
+            } else if keyboard_input.pressed(KeyCode::Up) {
+                actions.p1_bee_movement.y = 1.0;
+            } else if keyboard_input.pressed(KeyCode::Down) {
+                actions.p1_bee_movement.y = -1.0;
+            } else {
+                actions.p1_bee_movement.y = 0.0;
+            }
         }
-        actions.p1_movement.x = left_stick_x;
-        actions.p1_movement.y = left_stick_y;
-        actions.p1_bee_movement.x = right_stick_x;
-        actions.p1_bee_movement.y = right_stick_y;
     }
 }
